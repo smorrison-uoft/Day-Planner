63aacdf078a5b98b3f22737ea3f32a1dbc867965
stephanie morrison <stephmorr.to@gmail.com>
2020-06-25 10:28:05 -0400

    changes

--- Assets/calender.js
+++ Assets/calender.js
@@ -18,17 +18,23 @@ $(document).ready(function(){
        $(this).children(".description").addClass("past")
        $(this).children(".description").addClass("future")
     })
-
-
+   
 
     $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-    $("#hour-9 .description").val(localStorage.getItem("hour-10"));
-    $("#hour-9 .description").val(localStorage.getItem("hour-11"));
-    $("#hour-9 .description").val(localStorage.getItem("hour-12"));
-    $("#hour-9 .description").val(localStorage.getItem("hour-1"));
-    $("#hour-9 .description").val(localStorage.getItem("hour-2"));
-    $("#hour-9 .description").val(localStorage.getItem("hour-3"));
-    $("#hour-9 .description").val(localStorage.getItem("hour-4"));
-    $("#hour-9 .description").val(localStorage.getItem("hour-5"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-1 .description").val(localStorage.getItem("hour-1"));
+    $("#hour-2 .description").val(localStorage.getItem("hour-2"));
+    $("#hour-3 .description").val(localStorage.getItem("hour-3"));
+    $("#hour-4 .description").val(localStorage.getItem("hour-4"));
+    $("#hour-5 .description").val(localStorage.getItem("hour-5"));
     
 })
+$("#currentDay").text(moment().format('ddd, MMM YYYY'));
+
+
+
+
+
+
